9bd7907ab35b19fe44a9a1d134d5126e84d8e5a7
panupongchinn-sketch <Panupong.chinn@gmail.com>
2026-01-25 12:25:01 +0700

    fix: disable ssr for static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  nitro: {
-    preset: "static",
-  },
+
+  ssr: false, // ✅ สำคัญมาก กัน prerender / พังตอน build
+
+  nitro: { preset: "static" },
+
   css: ["./app/assets/css/main.css"],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
+
   runtimeConfig: {
     public: {
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_KEY: process.env.SUPABASE_KEY,
+      supabaseUrl: "",
+      supabaseKey: "",
     },
   },
 });
